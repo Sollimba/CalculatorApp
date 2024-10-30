ff0d5323d89ba03a1a1e324d7a9895856d7a2efd
Sollimba <134264168+Sollimba@users.noreply.github.com>
2024-10-31 00:01:18 +0300

    fin

---
+++ LabCalculator/LabCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LabCalculator.Models;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CalculatorController : ControllerBase
+{
+    [HttpPost]
+    public ActionResult<double> Calculate([FromBody] CalculatorModel model)
+    {
+        // Проверка на наличие первого операнда и операции
+        if (model.FirstOperand == 0 && string.IsNullOrEmpty(model.Operation))
+        {
+            return BadRequest("Model is required.");
+        }
+
+        double result = 0;
+
+        switch (model.Operation)
+        {
+            case "+":
+                result = model.FirstOperand + model.SecondOperand.GetValueOrDefault();
+                break;
+            case "-":
+                result = model.FirstOperand - model.SecondOperand.GetValueOrDefault();
+                break;
+            case "*":
+                result = model.FirstOperand * model.SecondOperand.GetValueOrDefault();
+                break;
+            case "/":
+                if (model.SecondOperand == 0) return BadRequest("Division by zero.");
+                result = model.FirstOperand / model.SecondOperand.GetValueOrDefault();
+                break;
+            case "^":
+                result = Math.Pow(model.FirstOperand, model.SecondOperand.GetValueOrDefault());
+                break;
+            case "sqrt":
+                result = Math.Sqrt(model.FirstOperand);
+                break;
+            case "sin":
+                result = Math.Sin(model.FirstOperand * (Math.PI / 180)); // Преобразование в радианы
+                break;
+            case "cos":
+                result = Math.Cos(model.FirstOperand * (Math.PI / 180)); // Преобразование в радианы
+                break;
+            case "tan":
+                result = Math.Tan(model.FirstOperand * (Math.PI / 180)); // Преобразование в радианы
+                break;
+            case "cot":
+                result = 1 / Math.Tan(model.FirstOperand * (Math.PI / 180)); // Преобразование в радианы
+                break;
+            default:
+                return BadRequest("Invalid operation");
+        }
+
+        // Округляем результат до одного знака после запятой
+        result = Math.Round(result, 1);
+
+        return Ok(result);
+    }
+}
---
+++ LabCalculator/LabCalculator/Models/CalculatorModel.cs
@@ -0,0 +1,11 @@
+﻿namespace LabCalculator.Models
+{
+    public class CalculatorModel
+    {
+        public double FirstOperand { get; set; }
+        public required string Operation { get; set; }
+
+        // Второй операнд может быть необязательным
+        public double? SecondOperand { get; set; } // Изменили на Nullable
+    }
+}
